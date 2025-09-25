598d54d02ea691ae7e3af4c88895b800088717c0
Andrikli <andilkiv15@gmail.com>
2025-09-26 01:03:35 +0300

    class fubonacci and function bool

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,30 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+class FibonacciNumber{
+    private int index;
+    private long value;
+    public FibonacciNumber(int index, long value){
+        this.index = index;
+        this.value = value;
     }
+    public int getIndex() {
+        return index;
+    }
+    public long getValue() {
+        return value;
+    }
+    public boolean CubeplusOne() {
+        long w= Math.round(Math.cbrt(value - 1));
+        return w * w * w+1==value;
+    }
+}
+public class Main {
+
+    public static void main(String[] args){
+        System.out.println("\n\t");
+
+    }
+
 }
