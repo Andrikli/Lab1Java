3bab5353fbfa176966674113c3b447b5ead7e7db
Andrikli <andilkiv15@gmail.com>
2025-09-26 10:22:07 +0300

    generate fibonacci numbers and check

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -15,15 +15,47 @@ class FibonacciNumber{
     public long getValue() {
         return value;
     }
-    public boolean CubeplusOne() {
+    public boolean cubePlusOne() {
         long w= Math.round(Math.cbrt(value - 1));
         return w * w * w+1==value;
     }
+
 }
+
 public class Main {
 
     public static void main(String[] args){
-        System.out.println("\n\t");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введіть кількість чисел Фібоначчі: ");
+        int n = sc.nextInt();
+        FibonacciNumber[] numbers = new FibonacciNumber[n];
+        int a=1, b=1;
+        for(int i=0;i<n;i++){
+            if(i==0 || i==1){
+                numbers[i] = new FibonacciNumber(i+ 1, 1);
+            }
+            else{
+                int c = a + b;
+                numbers[i]= new FibonacciNumber(i+1, c);
+                a=b;
+                b=c;
+            }
+        }
+        System.out.println("Числа Фібоначі:");
+        for(FibonacciNumber num : numbers){
+            System.out.println("["+num.getIndex()+"]="+num.getValue());
+        }
+        System.out.println("\nЧисла Фібоначчі виду w^3+1:");
+        boolean found = false;
+        for (FibonacciNumber num : numbers) {
+            if (num.cubePlusOne()) {
+                System.out.println("[" + num.getIndex() + "] = " + num.getValue());
+                found = true;
+            }
+        }
+        if (!found) {
+            System.out.println("Немає таких чисел серед перших " + n + " чисел Фібоначчі.");
+        }
 
     }
 
